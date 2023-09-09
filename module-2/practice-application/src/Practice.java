2f9800b510523384cd4b4cbb36e5c7e9ad248281
Olivia Fabreschi <olivia.fabreschi@gmail.com>
2023-09-09 10:27:07 +0200

    initial commit

---
+++ module-2/practice-application/src/Practice.java
@@ -0,0 +1,59 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class Practice extends JFrame implements ActionListener {
+
+    //instantiating the variables
+    private JButton button = new JButton("Push here to change colour");
+    private JButton buttonFinish = new JButton("Push here to stop");
+    private JLabel label = new JLabel("Hola amigos!");
+    private JTextField text = new JTextField("enter color here");
+
+    private static int nrOfPresses;
+
+    FlowLayout flowLayout = new FlowLayout();
+    Container contentPane = getContentPane();
+
+    public void actionPerformed(ActionEvent event)
+    {
+        if ((nrOfPresses % 3) == 0){
+            contentPane.setBackground(Color.RED);
+        }
+        else if ((nrOfPresses % 3) == 2){
+            contentPane.setBackground(Color.CYAN);
+        }
+        else
+            contentPane.setBackground(Color.PINK);
+        label.setText("colour changing!");
+        nrOfPresses++;
+    }
+
+    public Practice(){
+        //creating the content pane
+
+        contentPane.setLayout(flowLayout);
+        Color turquoise = new Color(75, 175, 175);
+        contentPane.setBackground(turquoise);
+        label.setFont(new Font("SansSerif", Font.BOLD | Font.ITALIC,35));
+
+        //adding the components
+        contentPane.add(label);
+        //spacing the inputs a bit
+        flowLayout.setHgap(50);
+       // contentPane.add(text);
+        contentPane.add(button);
+       // contentPane.add(buttonFinish);
+
+        button.addActionListener(this);
+        buttonFinish.addActionListener(this);
+
+        setSize(330, 300);
+        setVisible(true);
+
+    }
+    public static void main(String[] args) {
+        new Practice();
+    }
+}
